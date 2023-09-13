c8e7aacdfd6063b63567c8230749d84a3f81ce73
Nemiega <pvencius@gmail.com>
2023-09-13 13:09:22 +0300

    Added button event listeners and display results in HTML

--- javascript.js
+++ javascript.js
@@ -6,70 +6,65 @@ let positions = ["rock", "paper", "scissor"];
 
 // Function to get computer's choice
 let getComputerChoice = () => {
-    let computerChoice = positions[Math.floor(Math.random() * positions.length)]; 
-    return computerChoice;
-};
-
-// Function to get player's choice
-let playerSelect = () => {
-    let playerChoice = prompt("Please enter your weapon", "Rock, Paper, or Scissors");
-    return playerChoice ? playerChoice.toLowerCase() : null;
+  let computerChoice = positions[Math.floor(Math.random() * positions.length)];
+  return computerChoice;
 };
 
 // Function to play a round
 function playRound(playerChoice, computerChoice) {
-    if (((playerChoice == "paper") && (computerChoice == "rock")) || 
-    ((playerChoice == "scissor") && (computerChoice == "paper")) ||
-    ((playerChoice == "rock") && (computerChoice == "scissor"))) {
-        playerScore++;
-        roundWinner = 'player';
-    }
-    else if(playerChoice === computerChoice) {
-        roundWinner = "It's a tie";
-    }
-    else {
-        computerScore++;
-        roundWinner = 'computer';
-    }
-    updateScoreMessage(roundWinner, playerChoice, computerChoice);
+  if (
+    (playerChoice == "paper" && computerChoice == "rock") ||
+    (playerChoice == "scissor" && computerChoice == "paper") ||
+    (playerChoice == "rock" && computerChoice == "scissor")
+  ) {
+    playerScore++;
+    roundWinner = "player";
+  } else if (playerChoice === computerChoice) {
+    roundWinner = "It's a tie";
+  } else {
+    computerScore++;
+    roundWinner = "computer";
+  }
+  updateScoreMessage(roundWinner, playerChoice, computerChoice);
 }
 
 // Function to check if the game is over
 function isGameOver() {
-    return playerScore === 3 || computerScore === 3;
+  return playerScore === 5 || computerScore === 5;
 }
 
 // Function to display the winner of the game
 function setFinalMessage() {
-    if (playerScore > computerScore) {
-        return 'You won!';
-    } else if (playerScore < computerScore) {
-        return 'You lost...';
-    } else {
-        return "It's a tie!";
-    }
+  if (playerScore > computerScore) {
+    return 'You won!';
+  } else if (playerScore < computerScore) {
+    return 'You lost...';
+  } else {
+    return "It's a tie!";
+  }
 }
 
 // Function to update the score and round winner message
 function updateScoreMessage(roundWinner, playerChoice, computerChoice) {
-    console.log(`You chose ${playerChoice}, Computer chose ${computerChoice}`);
-    console.log(`Round Winner: ${roundWinner}`);
-    console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}`);
-    
-    if (isGameOver()) {
-        console.log(setFinalMessage());
-    }
+    const outputDiv = document.getElementById("output")
+    outputDiv.innerHTML = `You chose ${playerChoice}, Computer chose ${computerChoice}`;
+    outputDiv.innerHTML = `Round Winner: ${roundWinner}`;
+    outputDiv.innerHTML =`Player Score: ${playerScore}, Computer Score: ${computerScore}`;
+
+  if (isGameOver()) {
+    outputDiv.innerHTML = setFinalMessage();
+    playerScore = 0;
+    computerScore = 0
+  }
 }
 
-// Main game loop
-while (!isGameOver()) {
-    const playerChoice = playerSelect();
-    
-    if (!playerChoice) {
-        alert('Please enter a valid choice: Rock, Paper, or Scissors');
-        continue;
-    }
+// Add event listeners to the buttons
+const buttons = document.querySelectorAll('button');
 
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    const playerChoice = button.id;
     const computerChoice = getComputerChoice();
     playRound(playerChoice, computerChoice);
-}
+  });
+});
